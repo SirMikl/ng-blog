e2fe0d8e9baa5521da6a5b16fd647c60c1e62bab
SirMikl <sirmikl.aleinikov@gmail.com>
2021-02-24 12:08:29 +0300

    Added more new Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import {AngularFireStorageModule} from "@angular/fire/storage";
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
+      BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
